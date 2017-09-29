bd7f330459bd12bd152c93b98e3c56c50d3e6f26
Charlius <ASUS@Charlius.com>
2017-09-29 17:37:18 +0700

    Pembuatan SpalshActivity

--- app/src/main/java/com/uus/osa/sam/belajar/MainActivity.java
+++ app/src/main/java/com/uus/osa/sam/belajar/MainActivity.java
@@ -1,5 +1,7 @@
 package com.uus.osa.sam.belajar;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Handler handler;
+        handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+
+            }
+        },3000);
+
     }
 }
